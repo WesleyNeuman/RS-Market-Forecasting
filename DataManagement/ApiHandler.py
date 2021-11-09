574687b7b7fa417db68fb6796171a272d95d651a
Neuman, Wesley <Wesley.Neuman@gentex.com>
2021-11-09 13:21:32 -0500

    Revamped API calls and added retries

--- DataManagement/ApiHandler.py
+++ DataManagement/ApiHandler.py
@@ -1,22 +1,76 @@
 import requests
 import pandas as pd
 import json
+import time
+
+import DataManagement.ApiIteratedData as iter
 
 class ApiHandler(object):
     """Class for interacting with the Runescape APIs"""
 
-    # 
-    def ImportByCategory(self, category, alpha, page):
-        endpoint = "https://services.runescape.com/m=itemdb_rs/api/catalogue/items.json"
+    endpoint = "https://services.runescape.com/m=itemdb_rs/api/catalogue/items.json"
+    short_delay = 5
+    long_delay = 120
+
+    # Lowest level of pull for category builder
+    def ImportByChunk(self, category, alpha, page, recursion_level=0):
         params = {
-            'category' : '0',
-            'alpha' : 'a',
-            'page' : '1'
+            'category' : category,
+            'alpha' : alpha,
+            'page' : page
             }
-        response = requests.get(endpoint, params)
+        response = requests.get(self.endpoint, params)
         jtext = response.json()
+
+        # Actions for if the response is bad
+        if jtext == 'BAD' & recursion_level < 5:
+            time.sleep(short_delay)
+            recursion_df = self.ImportByChunk(category, alpha, page, recursion_level+1)
+            return recursion_df
+        elif jtext == 'BAD' & recursion_level == 5:
+            time.sleep(long_delay)
+            recursion_df = self.ImportByChunk(category, alpha, page, recursion_level+1)
+            return recursion_df
+        elif jtext == 'BAD' & recursion_level > 5:
+            return None
+
         response_df = pd.DataFrame.from_dict(jtext['items'])
         response_df = response_df[['id','type','name','members']]
         return response_df
+
+
+    # Determines the number of items for a given category by letter
+    def DeterminePageCount(self, category, recursion_level=0):
+        params = {
+            'category' : category
+            }
+        response = requests.get(self.endpoint, params)
+        jtext = response.json()
+
+        # Actions for if the response is bad
+        if jtext == 'BAD' & recursion_level < 5:
+            time.sleep(short_delay)
+            recursion_dict = self.DeterminePageCount(category, alpha, recursion_level+1)
+            return recursion_dict
+        elif jtext == 'BAD' & recursion_level == 5:
+            time.sleep(long_delay)
+            recursion_dict = self.DeterminePageCount(category, alpha, recursion_level+1)
+            return recursion_dict
+        elif jtext == 'BAD' & recursion_level > 5:
+            return None
+
+        cat_dict = jtext['alpha']
+        return cat_dict
+
+
+    # Import the entirety of IDs for a given category and letter
+    def ImportByCategory(self, category, alpha, total_pages):
+        df = pd.DataFrame()
+        for i in range(1, total_pages):
+            df = df.append(ImportByChunk(category, alpha, i))
+        return df
+
+
+
 
 
---
+++ DataManagement/ApiIteratedData.py
@@ -0,0 +1,11 @@
+# Generate iterable sets of data for API querying
+def get_alpha():
+    return ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+def get_categories():
+    return list(range(0,41))
+
+
+
+
+
